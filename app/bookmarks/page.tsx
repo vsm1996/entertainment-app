5a4f3aded18dd382de705afdad564f4d8799d1d4
Vanessa Martin <39349678+vsm1996@users.noreply.github.com>
2025-11-21 21:49:04 +0000

    remove unnecessary lines of code

--- app/bookmarks/page.tsx
+++ app/bookmarks/page.tsx
@@ -1,63 +1,54 @@
 "use client";
 
-import { useState } from "react";
-
 import CardData from "@/data.json";
 import Card from "@/component/ui/card";
 import Input from "@/component/ui/input";
 
 const Bookmarks = () => {
-  const [cardData, setData] = useState<any[]>(CardData);
-
   return (
     <div className="p-200 sm:p-300 md:p-400 col-start-1 flex flex-col gap-300">
       <Input />
       <h1 className="text-1">Bookmarked Movies</h1>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 lg:pr-200 gap-200">
-        {cardData
-          .filter(
-            (data) => data.isBookmarked === true && data.category === "Movie"
+        {CardData.filter(
+          (data) => data.isBookmarked === true && data.category === "Movie"
+        ).map(
+          (
+            { title, thumbnail, year, category, rating, isBookmarked },
+            index
+          ) => (
+            <Card
+              key={index}
+              title={title}
+              thumbnail={thumbnail}
+              year={year}
+              category={category}
+              isBookmarked={isBookmarked}
+              rating={rating}
+            />
           )
-          .map(
-            (
-              { title, thumbnail, year, category, rating, isBookmarked },
-              index
-            ) => (
-              <Card
-                key={index}
-                title={title}
-                thumbnail={thumbnail}
-                year={year}
-                category={category}
-                isBookmarked={isBookmarked}
-                rating={rating}
-              />
-            )
-          )}
+        )}
       </div>
       <h1 className="text-1">Bookmarked TV Series</h1>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 lg:pr-200 gap-200">
-        {cardData
-          .filter(
-            (data) =>
-              data.isBookmarked === true && data.category === "TV Series"
+        {CardData.filter(
+          (data) => data.isBookmarked === true && data.category === "TV Series"
+        ).map(
+          (
+            { title, thumbnail, year, category, rating, isBookmarked },
+            index
+          ) => (
+            <Card
+              key={index}
+              title={title}
+              thumbnail={thumbnail}
+              year={year}
+              category={category}
+              isBookmarked={isBookmarked}
+              rating={rating}
+            />
           )
-          .map(
-            (
-              { title, thumbnail, year, category, rating, isBookmarked },
-              index
-            ) => (
-              <Card
-                key={index}
-                title={title}
-                thumbnail={thumbnail}
-                year={year}
-                category={category}
-                isBookmarked={isBookmarked}
-                rating={rating}
-              />
-            )
-          )}
+        )}
       </div>
     </div>
   );
--- app/tv-series/page.tsx
+++ app/tv-series/page.tsx
@@ -1,47 +1,32 @@
 "use client";
 
-import { useState } from "react";
-
 import CardData from "@/data.json";
 import Card from "@/component/ui/card";
 import Input from "@/component/ui/input";
 
 const TVSeries = () => {
-  const [cardData, setData] = useState<any[]>(CardData);
-  const [searchTerm, setTerm] = useState<string>("");
-
   return (
     <div className="p-200 sm:p-300 md:p-400 col-start-1 flex flex-col gap-300">
       <Input />
 
       <h1 className="text-1">TV Series</h1>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 lg:pr-200 gap-200">
-        {cardData
-          .filter((data) => data.category === "TV Series")
-          .map(
-            (
-              {
-                title,
-                thumbnail,
-                year,
-                category,
-                rating,
-                isBookmarked,
-                isTrending,
-              },
-              index
-            ) => (
-              <Card
-                key={index}
-                title={title}
-                thumbnail={thumbnail}
-                year={year}
-                category={category}
-                isBookmarked={isBookmarked}
-                rating={rating}
-              />
-            )
-          )}
+        {CardData.filter((data) => data.category === "TV Series").map(
+          (
+            { title, thumbnail, year, category, rating, isBookmarked },
+            index
+          ) => (
+            <Card
+              key={index}
+              title={title}
+              thumbnail={thumbnail}
+              year={year}
+              category={category}
+              isBookmarked={isBookmarked}
+              rating={rating}
+            />
+          )
+        )}
       </div>
     </div>
   );
