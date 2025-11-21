2a3acda5e089e2b8482f6597d3385e4f98070ac5
Vanessa Martin <39349678+vsm1996@users.noreply.github.com>
2025-11-21 21:58:01 +0000

    fix nav icons

--- component/ui/icons/HomeIcon.tsx
+++ component/ui/icons/HomeIcon.tsx
@@ -12,7 +12,8 @@ const HomeIcon = ({ fill = "#5A698F" }: Icon) => {
     >
       <path
         d="M8 0H1C.4 0 0 .4 0 1v7c0 .6.4 1 1 1h7c.6 0 1-.4 1-1V1c0-.6-.4-1-1-1Zm0 11H1c-.6 0-1 .4-1 1v7c0 .6.4 1 1 1h7c.6 0 1-.4 1-1v-7c0-.6-.4-1-1-1ZM19 0h-7c-.6 0-1 .4-1 1v7c0 .6.4 1 1 1h7c.6 0 1-.4 1-1V1c0-.6-.4-1-1-1Zm0 11h-7c-.6 0-1 .4-1 1v7c0 .6.4 1 1 1h7c.6 0 1-.4 1-1v-7c0-.6-.4-1-1-1Z"
-        className={`${fillColor} transition-all duration-400 ease-out group-hover:fill-red-500`}
+        fill={fill}
+        className={`${fillColor} transition-all duration-400 ease-out group-hover:fill-red-500 active:fill-white`}
       />
     </svg>
   );
